7eb8374ef478272c7b493fb094ed13082751583c
saliniag <salini.agarwal11@gmail.com>
2017-12-08 11:59:38 -0800

    mvc project

---
+++ BusinessLayer/EmployeeBusinessLayer.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BusinessLayer
+{
+    public class EmployeeBusinessLayer
+    {
+        // access the data
+        public IEnumerable<Employee> Employees{
+            get
+            {
+                string connString = ConfigurationManager.ConnectionStrings["Dbcs"].ConnectionString;
+                //list to store all the data
+                List<Employee> employees = new List<Employee>();
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+
+                    SqlCommand cmd = new SqlCommand("spGetAllEmployee", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    conn.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                   
+                    while (rdr.Read())
+                    {
+                        Employee current = new Employee();
+                        current.Id = Convert.ToInt32(rdr["Id"]);
+                        current.FirstName = rdr["FirstName"].ToString();
+                        current.LastName = rdr["LastName"].ToString();
+                        current.City = rdr["City"].ToString();
+                        employees.Add(current);
+                    }
+                }
+                return employees;
+            }
+        }
+//method to submit the data in the database to create a new employee
+        public void AddNewEmployee(Employee emp)
+        {
+            //get the connectionString
+            string connString = ConfigurationManager.ConnectionStrings["Dbcs"].ConnectionString;
+            //spAddNewEmployee is procedure to add a new employee
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                SqlCommand cmd = new SqlCommand("spAddNewEmployee", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                //add the parameters
+                //SqlParameter paraId = new SqlParameter();
+                //paraId.ParameterName= "@Id";
+                //paraId.Value = emp.Id;
+                //cmd.Parameters.Add(paraId);
+                SqlParameter paraFirst = new SqlParameter();
+                paraFirst.ParameterName = "@FirstName";
+                paraFirst.Value = emp.FirstName;
+                cmd.Parameters.Add(paraFirst);
+                SqlParameter paraLast = new SqlParameter();
+                paraLast.ParameterName = "@LastName";
+                paraLast.Value = emp.LastName;
+                cmd.Parameters.Add(paraLast);
+                SqlParameter paraCity = new SqlParameter();
+                paraCity.ParameterName = "@City";
+                paraCity.Value = emp.City;
+                cmd.Parameters.Add(paraCity);
+
+                //open the connection
+                conn.Open();
+                //execute the query
+                cmd.ExecuteNonQuery();
+                
+
+                
+            }
+        }
+
+        //method to update the employee data
+        public void saveUpdatedEmployeeInfo(Employee emp)
+        {
+            //get the connectionString
+            string connString = ConfigurationManager.ConnectionStrings["Dbcs"].ConnectionString;
+            //spAddNewEmployee is procedure to add a new employee
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                SqlCommand cmd = new SqlCommand("spUpdateEmployee", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                //add the parameters
+                SqlParameter paraId = new SqlParameter();
+                paraId.ParameterName = "@Id";
+                paraId.Value = emp.Id;
+                cmd.Parameters.Add(paraId);
+                SqlParameter paraFirst = new SqlParameter();
+                paraFirst.ParameterName = "@FirstName";
+                paraFirst.Value = emp.FirstName;
+                cmd.Parameters.Add(paraFirst);
+                SqlParameter paraLast = new SqlParameter();
+                paraLast.ParameterName = "@LastName";
+                paraLast.Value = emp.LastName;
+                cmd.Parameters.Add(paraLast);
+                SqlParameter paraCity = new SqlParameter();
+                paraCity.ParameterName = "@City";
+                paraCity.Value = emp.City;
+                cmd.Parameters.Add(paraCity);
+
+                //open the connection
+                conn.Open();
+                //execute the query
+                cmd.ExecuteNonQuery();
+
+
+
+            }
+
+        }
+
+        //method to delete the record
+
+        public void DeleteEmployee(int id)
+        {
+             //get the connectionString
+            string connString = ConfigurationManager.ConnectionStrings["Dbcs"].ConnectionString;
+            //spAddNewEmployee is procedure to add a new employee
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                SqlCommand cmd = new SqlCommand("spDeleteEmployee", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                // add the parameters
+                SqlParameter paraId = new SqlParameter();
+                paraId.ParameterName = "@Id";
+                paraId.Value = id;
+                cmd.Parameters.Add(paraId);
+                //open connection
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ MVCdataAccessUsingBusinessObjectAsModel/Controllers/EmployeeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer;
+
+namespace MVCdataAccessUsingBusinessObjectAsModel.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+        public ActionResult Index()
+        {
+            EmployeeBusinessLayer Bl = new EmployeeBusinessLayer();
+            List<Employee> ems = Bl.Employees.ToList();
+            return View(ems);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+          
+            return View();
+        }
+
+        //when the user wil submit the create employee
+        //post action method with call
+        [HttpPost]
+        public ActionResult Create(Employee em)
+        {
+            EmployeeBusinessLayer Bl = new EmployeeBusinessLayer();
+            Bl.AddNewEmployee(em);
+            //will redirect the user to the index page
+            return RedirectToAction("Index");
+        }
+        //controller for edit a employee
+        //when user clicks edit --a get request will hit this controller
+        [HttpGet]
+        [ActionName("Edit")]
+        public ActionResult Edit_get(int Id)
+        {
+            EmployeeBusinessLayer bl = new EmployeeBusinessLayer();
+            Employee emp = bl.Employees.Single(x => x.Id == Id);
+            //return form to edit the employee data
+            return View(emp);
+        }
+        //post action method to update the employee
+        //
+        [HttpPost]
+        [ActionName("Edit")]
+        public ActionResult Edit_post([Bind(Include="Id,FirstName,City")]Employee emp){
+            // not giving access to user to change the last name
+            //get the name from business layer
+            EmployeeBusinessLayer bl= new EmployeeBusinessLayer();
+            emp.LastName = bl.Employees.Single(x => x.Id == emp.Id).LastName;
+            if (ModelState.IsValid)
+            {
+                //update the database
+                bl.saveUpdatedEmployeeInfo(emp);
+                return RedirectToAction("Index");
+            }
+            return View(emp);
+
+        }
+
+        //Deleting a Record using Pist
+        [HttpPost]
+        public ActionResult Delete(int Id) //it will bind automatically by parameter binding on submit
+        {
+            EmployeeBusinessLayer bl = new EmployeeBusinessLayer();
+            bl.DeleteEmployee(Id);
+            return RedirectToAction("Index");
+        }
+	}
+}
